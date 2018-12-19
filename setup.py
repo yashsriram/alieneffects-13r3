8730eea71bc3669231e4567fd82b37bc2117bacb
Yashasvi Sriram <yash.3997@gmail.com>
2018-12-20 02:47:08 +0530

    names the application alien-effects-13r3

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setup(
-    name='alieneffects',
+    name='alien-effects-13r3',
     version='0.1',
-    scripts=['alieneffects/alien-effects'],
+    scripts=['alieneffects/alien-effects-13r3'],
     packages=['alieneffects'],
     author='Yashasvi Sriram',
     author_email='yash.3997@gmail.com',
